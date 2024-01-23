c4eacac4f068f5675734a34974d18f5b9310147b
Luis Armendariz <lmarmend99@gmail.com>
2024-01-22 17:30:41 -0800

    ADD: support for DynamoDB

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import asyncio
 import base64
+import boto3
+from dotenv import load_dotenv
 import os
 import spotipy
 import logging
@@ -7,11 +9,33 @@ import re
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 from spotipy.oauth2 import SpotifyOAuth
-from dotenv import load_dotenv
+
 
 # Load environment variables and configure logging
 load_dotenv()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('SpotifySkunk')  
+
+def save_playlist_to_dynamodb(chat_id, playlist_id):
+    try:
+        table.put_item(Item={'chat_id': str(chat_id), 'playlist_id': playlist_id})
+    except Exception as e:
+        logging.error(f"Error saving to DynamoDB: {e}")
+
+
+def get_playlist_from_dynamodb(chat_id):
+    try:
+        response = table.get_item(Key={'chat_id': str(chat_id)})
+        if 'Item' in response:
+            playlist_id = response['Item'].get('playlist_id')
+            return playlist_id
+        return None
+    except Exception as e:
+        logging.error(f"Error retrieving from DynamoDB: {e}")
+        return None
 
 # Spotify API credentials and initialization
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
@@ -19,9 +43,6 @@ SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 sp_oauth = SpotifyOAuth(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI, 
                         scope='playlist-modify-public ugc-image-upload')
-
-# Global variables
-chat_playlists = {}  
 spotify_link_pattern = r'https://open\.spotify\.com/track/([a-zA-Z0-9]+)'
 
 # -----------------------------------------------
@@ -60,25 +81,26 @@ def create_spotify_playlist(playlist_name):
 async def handle_playlist_image(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
 
-    if context.user_data.get(chat_id, {}).get('awaiting_playlist_image') or (chat_id in chat_playlists and context.user_data.get(chat_id, {}).get('changing_playlist_image')):
+    if context.user_data.get(chat_id, {}).get('awaiting_playlist_image') or (context.user_data.get(chat_id, {}).get('changing_playlist_image')):
         photo = update.message.photo[-1]  
         await update.message.reply_text("Processing your image, please wait...")
+        playlist_id = get_playlist_from_dynamodb(chat_id)
+        if not playlist_id:
+            await update.message.reply_text("No playlist found for this chat.")
+            return
         try:
             photo_file = await context.bot.get_file(photo.file_id)
             photo_bytes = await photo_file.download_as_bytearray()
             base64_image = base64.b64encode(photo_bytes).decode('utf-8')
             async def upload_image():
-                playlist_id = chat_playlists[chat_id]['playlist_id']
                 sp = spotipy.Spotify(auth_manager=sp_oauth)
                 sp.playlist_upload_cover_image(playlist_id, base64_image)
             await asyncio.wait_for(upload_image(), timeout=30)
             await update.message.reply_text("Playlist cover image set successfully!")
-            if chat_id in chat_playlists and 'playlist_id' in chat_playlists[chat_id]:
-                playlist_id = chat_playlists[chat_id]['playlist_id']
-                playlist_url = f"https://open.spotify.com/playlist/{playlist_id}"
-                await update.message.reply_text(f"Here's your playlist link: {playlist_url}")
-            else:
-                await update.message.reply_text("Error: Currently unable to retrieve the playlist link.")
+       
+            playlist_url = f"https://open.spotify.com/playlist/{playlist_id}"
+            await update.message.reply_text(f"Here's your playlist link: {playlist_url}")
+          
             context.user_data[chat_id]['awaiting_playlist_image'] = False
         except asyncio.TimeoutError:
             await update.message.reply_text("Image upload timed out. Please try again.")
@@ -92,10 +114,9 @@ async def handle_playlist_name(update: Update, context: CallbackContext) -> None
     chat_id = update.message.from_user.id
 
     if context.user_data.get(chat_id, {}).get('changing_playlist_name'):
-        if chat_id in chat_playlists:
+        playlist_id = get_playlist_from_dynamodb(chat_id)
+        if playlist_id:
             new_name = update.message.text.strip()
-            if chat_id in chat_playlists and 'playlist_id' in chat_playlists[chat_id]:
-                playlist_id = chat_playlists[chat_id]['playlist_id']
             if change_spotify_playlist_name(playlist_id, new_name):
                 await update.message.reply_text(f"Playlist name changed to: {new_name}")
             else:
@@ -103,11 +124,10 @@ async def handle_playlist_name(update: Update, context: CallbackContext) -> None
             context.user_data[chat_id]['changing_playlist_name'] = False
         else:
             await update.message.reply_text("Make sure you have a playlist created before changing the name.")
-    if (chat_id in context.user_data and context.user_data[chat_id].get('creating_playlist') 
-        and chat_id not in chat_playlists):
+    elif context.user_data.get(chat_id, {}).get('creating_playlist'):
         playlist_name = update.message.text.strip()
         playlist_id = create_spotify_playlist(playlist_name)
-        chat_playlists[chat_id] = {'playlist_id': playlist_id}
+        save_playlist_to_dynamodb(chat_id, playlist_id)
         context.user_data[chat_id]['awaiting_playlist_image'] = True
         await update.message.reply_text(f"Created new playlist: {playlist_name}. Now please send me an cool image to set as your playlist cover 😎.")
         context.user_data[chat_id]['creating_playlist'] = False
@@ -119,18 +139,17 @@ async def handle_spotify_links(update: Update, context: CallbackContext) -> None
     message_text = update.message.text
     match = re.search(spotify_link_pattern, message_text)
     if chat_id in context.user_data and context.user_data[chat_id].get('creating_playlist'):
-        # If the user is still flagged as creating a playlist, ignore Spotify links
         return
-    if match and chat_id in chat_playlists:
+    playlist_id = get_playlist_from_dynamodb(chat_id)  
+    if match and playlist_id:
         track_id = match.group(1)
-        playlist_id = chat_playlists[chat_id]['playlist_id']
         if add_track_to_spotify_playlist(playlist_id, track_id):
                 await update.message.reply_text("Added Spotify track to your playlist!")
                 await update.message.reply_text("🦨 ❤️ 🎶")
         else:
             await update.message.reply_text("Failed to add the track. Make sure you have the correct permissions or that the Playlist still exist.")
     else:
-        await update.message.reply_text("Please create a new playlist using /createplaylist or specify an existing playlist.")
+        await update.message.reply_text("Please create a new playlist using /createplaylist ")
 
 # -----------------------------------------------
 # Telegram Command Handlers
@@ -138,7 +157,8 @@ async def handle_spotify_links(update: Update, context: CallbackContext) -> None
     
 async def change_playlist_image(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
-    if chat_id in chat_playlists:
+    playlist_id = get_playlist_from_dynamodb(chat_id)
+    if playlist_id:
         await update.message.reply_text("Please send the new image for your playlist:")
         context.user_data[chat_id] = {'changing_playlist_image': True}
     else:
@@ -146,7 +166,8 @@ async def change_playlist_image(update: Update, context: CallbackContext) -> Non
 
 async def change_playlist_name(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
-    if chat_id in chat_playlists:
+    playlist_id = get_playlist_from_dynamodb(chat_id)
+    if playlist_id:
         await update.message.reply_text("Please enter the new name for your playlist:")
         context.user_data[chat_id] = {'changing_playlist_name': True}
     else:
@@ -154,7 +175,8 @@ async def change_playlist_name(update: Update, context: CallbackContext) -> None
 
 async def create_playlist(update: Update, context: CallbackContext) -> bool:
     chat_id = update.effective_chat.id
-    if chat_id in chat_playlists:
+    playlist_id = get_playlist_from_dynamodb(chat_id)
+    if playlist_id:
         await update.message.reply_text("A playlist has already been created for this chat.")
         return
     context.user_data[chat_id] = context.user_data.get(chat_id, {})
@@ -175,21 +197,30 @@ async def help_command(update: Update, context: CallbackContext) -> None:
     )
     await update.message.reply_text(help_text)
 
+
 async def reset_playlist(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
-    if chat_id in chat_playlists:
-        del chat_playlists[chat_id]
+    # Delete the playlist entry from DynamoDB
+    try:
+        table.delete_item(Key={'chat_id': str(chat_id)})
+    except Exception as e:
+        logging.error(f"Error deleting from DynamoDB: {e}")
+        await update.message.reply_text("Failed to reset the playlist in the database.")
+        return
+    # Clear any related flags in context.user_data
     context.user_data[chat_id] = {}
     await update.message.reply_text("The current playlist has been reset. You can create a new playlist with /createplaylist.")
 
+
 async def send_playlist_link(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
-    if chat_id in chat_playlists and 'playlist_id' in chat_playlists[chat_id]:
-        playlist_id = chat_playlists[chat_id]['playlist_id']
+    playlist_id = get_playlist_from_dynamodb(chat_id)
+    if playlist_id:
         playlist_url = f"https://open.spotify.com/playlist/{playlist_id}"
         await update.message.reply_text(f"Here's your playlist link: {playlist_url}")
     else:
         await update.message.reply_text("No playlist found for this chat.")
+
 
 async def start(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text("Hiya! I'm your Spotify Skunk bot 🦨. /createplaylist so I can add spotify links as tracks in your playlist! ")
